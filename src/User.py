a51f84d43db85625cd8bfd326061f8544e71a02a
nickiiinicole <nicki.nicoledc04@gmail.com>
2025-11-21 11:26:20 +0100

    ej49

---
+++ src/User.py
@@ -0,0 +1,15 @@
+class User: 
+    # el numero de tarjeta sigue el la formual de Luhn
+    def __init__(self,name,username,password, card_number):
+        self.name= name
+        self.username= username
+        self.password= password
+        self.card_number= card_number
+
+
+
+    def check_sum(self):
+        '''La fórmula de Lunh'''
+        if len(self.card_number)<=1 : 
+            raise
+        
