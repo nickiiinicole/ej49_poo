350f018fb6f8270730c8ec405fe0d3a473a0e6fe
nickiiinicole <nicki.nicoledc04@gmail.com>
2025-11-22 10:30:02 +0100

    falta hacer la segunda validadacion de la tarjeta

--- src/User.py
+++ src/User.py
@@ -1,3 +1,4 @@
+import Gamin_exception 
 class User: 
     # el numero de tarjeta sigue el la formual de Luhn
     def __init__(self,name,username,password, card_number):
@@ -10,6 +11,21 @@ class User:
 
     def check_sum(self):
         '''La fórmula de Lunh'''
-        if len(self.card_number)<=1 : 
-            raise
+        if len(self.card_number.strip())<=1 and not self.card_number.isdigit() : 
+            raise Gamin_exception
         
+        digits_odd = self.card_number[::2]
+        digits_odd = [int(digit) for digit in digits_odd]
+        digits_odd = [digit*2 for digit in digits_odd]
+        
+        for digit in digits_odd:
+            if digit>9:
+                digit-=9
+        
+        if digits_odd.sum()%10 ==0 :
+            return digits_odd
+        else: 
+            raise Gamin_exception
+
+        
+
