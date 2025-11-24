5e156f87f3564dcaf293a59abfb992143690ee43
nickiiinicole <nicki.nicoledc04@gmail.com>
2025-11-24 09:43:39 +0100

    hecho . pongo los get?

--- src/User.py
+++ src/User.py
@@ -11,6 +11,24 @@ class User:
 
     def check_sum(self):
         '''La fórmula de Lunh'''
-        if len(self.card_number)<=1 : 
-            raise
+        if len(self.card_number.strip())<13 or not self.card_number.isdigit() : 
+            raise Gamin_exception("Invalid card number")
         
+        # digits_odd = self.card_number[::2]
+        # digits_odd = [int(digit) for digit in digits_odd]
+        # digits_odd = [digit*2 for digit in digits_odd]
+        # hacerlo en una linea 
+        digits_odd = [int(digit) * 2 for digit in self.card_number[::2]]
+        
+        modified_digits = [digit - 9 if digit > 9 else digit for digit in digits_odd]
+        remaining_digits = [int(digit) for digit in self.card_number[1::2]]
+
+        total_digits = sum(modified_digits) + sum(remaining_digits)
+        
+        if total_digits%10 ==0 :
+            return True
+        else: 
+            raise Gamin_exception("ERROR, INVALID COMPROBATION LUHN:/// ")
+
+        
+
